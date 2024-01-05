5394255211be16ef426f7ca87e00aedc1d0a5428
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-01-05 16:36:17 +0200

    rout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import { useEffect, lazy, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
 import { refreshUser } from 'redux/operations';
-import { PrivateRoute } from './PrivateRoute';
+import { PrivateRoute } from './PrivetRoute';
 import { RestrictedRoute } from './RestrictedRoute';
 import { Loader } from './Loader/Loader';
 import Layout from './Layout';
