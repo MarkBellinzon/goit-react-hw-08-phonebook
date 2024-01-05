1bf13afa1782bf10510b0c15876a2d066fea8eda
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-01-05 16:32:02 +0200

    privetrout

---
+++ src/components/PrivetRoute.js
@@ -0,0 +1,8 @@
+import { Navigate } from 'react-router-dom';
+import { useAuth } from 'redux/useAuth';
+
+export const PrivateRoute = ({ component: Component, adressTo = '/' }) => {
+  const { isLoggedIn, isRefreshing } = useAuth();
+  const sendTo = !isLoggedIn && !isRefreshing;
+  return sendTo ? <Navigate to={adressTo} /> : Component;
+};
---
+++ src/components/RestrictedRoute.js
@@ -0,0 +1,8 @@
+import { useAuth } from 'redux/useAuth';
+import { Navigate } from 'react-router-dom';
+
+
+export const RestrictedRoute = ({ component: Component, adressTo = '/' }) => {
+  const { isLoggedIn } = useAuth();
+  return isLoggedIn ? <Navigate to={adressTo} /> : Component;
+};
