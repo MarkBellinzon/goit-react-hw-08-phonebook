6a03c000a565b68390305d08f0d007fe49e6676b
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-01-07 18:36:59 +0200

    text2

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -5,18 +5,18 @@ export default function HomePage() {
     <div className={css.container}>
       <div>
         <h1 className={css.title}>Create Edit Store</h1>
-        <p className={css.text}>
+        {/* <p className={css.text}>
           This site helps you to create and save phone numbers of your friends,
           acquaintances and clients. To get started, you need to first register
           and then log in to your account. Then you have the opportunity to
           create a contact and his phone number. They will be saved and you can
           delete them. Thanks to registration, your contacts will be protected.
           Good luck!
-        </p>
+        </p> */}
         {/* <p className={css.text}>you will dance with joy</p> */}
       </div>
 
-      <img src="/public/cont.png" alt="contacts" />
+      <img className={css.img} src="/public/cont.png" alt="contacts" />
 
       {/* <iframe
         className={css.video}
