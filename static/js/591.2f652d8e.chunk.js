cd63dbdd0ee6c950263f1a658b7c86097171c500
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-01-07 16:42:24 +0000

    Deploying to gh-pages from @ MarkBellinzon/goit-react-hw-08-phonebook@4b50d382ae7791c9ed30f18c5b80b1b4b2ff8492 🚀

--- static/js/591.2f652d8e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[591],{591:function(e,t,c){c.r(t),c.d(t,{default:function(){return s}});var n="HomePage_container__8GNiX",a="HomePage_title__DWlEo",o="HomePage_img__NLnch",i=c(184);function s(){return(0,i.jsxs)("div",{className:n,children:[(0,i.jsx)("div",{children:(0,i.jsx)("h1",{className:a,children:"Create Edit Store"})}),(0,i.jsx)("img",{className:o,src:"/public/cont.png",alt:"contacts"})]})}}}]);
-//# sourceMappingURL=591.2f652d8e.chunk.js.map
